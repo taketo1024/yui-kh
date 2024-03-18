4fedb075cb5f214b114345fd14b38f6943b46a16
Taketo Sano <taketo1024@gmail.com>
2024-03-18 15:09:54 +0900

    CKhI::tau_state

--- src/lib/khi/complex.rs
+++ src/lib/khi/complex.rs
@@ -1,10 +1,15 @@
+use std::collections::HashSet;
+
+use yui::bitseq::Bit;
 use yui::{Ring, RingOps};
+use yui_link::State;
 
 use crate::v1::cube::KhCube;
 use super::InvLink;
 
 pub struct KhIComplex<R>
 where R: Ring, for<'a> &'a R: RingOps<R> { 
+    l: InvLink,
     c0: KhCube<R>, // lower
     c1: KhCube<R>  // upper
 }
@@ -13,10 +18,26 @@ impl<R> KhIComplex<R>
 where R: Ring, for<'a> &'a R: RingOps<R> { 
     pub fn new(l: &InvLink, h: &R) -> Self { 
         assert_eq!(R::one() + R::one(), R::zero(), "char(R) != 2");
+
+        let l = l.clone();
         let t = R::zero();
         let c0 = KhCube::new(l.link(), h, &t);
         let c1 = KhCube::new(l.link(), h, &t);
-        Self { c0, c1 }
+
+        Self { l, c0, c1 }
+    }
+
+    fn tau_state(&self, s: State) -> State { 
+        let n = s.len();
+        let xs = s.iter().enumerate().filter_map(|(i, b)| 
+            b.is_one().then_some(i)
+        ).map(|i| 
+            self.l.inv_x(i)
+        ).collect::<HashSet<_>>();
+
+        State::from_iter((0..n).map(|i| 
+            if xs.contains(&i) { Bit::Bit1 } else { Bit::Bit0 }
+        ))
     }
 }
 
@@ -28,7 +49,7 @@ mod tests {
     use super::*;
  
     #[test]
-    fn init() { 
+    fn tau_state() { 
         let l = InvLink::new(
             Link::from_pd_code([[1,5,2,4],[3,1,4,6],[5,3,6,2]]), 
             [(1,5), (2,4)]
@@ -37,5 +58,45 @@ mod tests {
         type R = FF<2>;
         let h = R::zero();
         let c = KhIComplex::new(&l, &h);
+
+        assert_eq!(
+            c.tau_state(State::from([0,0,0])), 
+            State::from([0,0,0])
+        );
+        
+        assert_eq!(
+            c.tau_state(State::from([1,0,0])), 
+            State::from([1,0,0])
+        );
+
+        assert_eq!(
+            c.tau_state(State::from([0,1,0])), 
+            State::from([0,0,1])
+        );
+
+        assert_eq!(
+            c.tau_state(State::from([0,0,1])), 
+            State::from([0,1,0])
+        );
+
+        assert_eq!(
+            c.tau_state(State::from([1,1,0])), 
+            State::from([1,0,1])
+        );
+
+        assert_eq!(
+            c.tau_state(State::from([1,0,1])), 
+            State::from([1,1,0])
+        );
+
+        assert_eq!(
+            c.tau_state(State::from([0,1,1])), 
+            State::from([0,1,1])
+        );
+
+        assert_eq!(
+            c.tau_state(State::from([1,1,1])), 
+            State::from([1,1,1])
+        );
     }
 }
