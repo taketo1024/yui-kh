0bc4f0e41f8513484cd3c11a2039b5b10b0d5d51
Taketo Sano <taketo1024@gmail.com>
2024-03-18 14:35:33 +0900

    InvLink::inv_e, inv_x

--- src/lib/khi/inv_link.rs
+++ src/lib/khi/inv_link.rs
@@ -1,12 +1,14 @@
 use std::collections::HashMap;
 
+use itertools::Itertools;
 use yui_link::{Edge, Link};
 
 // Involutive link
 #[derive(Debug, Clone)]
 pub struct InvLink { 
     link: Link,
-    e_map: HashMap<Edge, Edge>
+    e_map: HashMap<Edge, Edge>,
+    x_map: HashMap<usize, usize>
 }
 
 impl InvLink { 
@@ -24,25 +26,63 @@ impl InvLink {
             edges.remove(&e1);
             edges.remove(&e2);
         }
-        
+
         for e in edges { 
             e_map.insert(e, e);
         }
 
-        Self { link, e_map }
+        let mut x_map = HashMap::new();
+
+        // TODO? check resolution
+        for (i, x) in link.data().iter().enumerate() { 
+            let edges = x.edges().map(|e| e_map.get(&e).unwrap());
+            let find = link.data().iter().find_position(|y|
+                edges.iter().all(|e| y.edges().contains(e))
+            );
+
+            assert!(find.is_some());
+
+            let j = find.unwrap().0;
+            x_map.insert(i, j);
+            x_map.insert(j, i);
+        }
+
+        Self { link, e_map, x_map }
+    }
+
+    pub fn inv_e(&self, e: Edge) -> Edge { 
+        self.e_map.get(&e).cloned().unwrap()
+    }
+
+    pub fn inv_x(&self, i: usize) -> usize { 
+        self.x_map.get(&i).cloned().unwrap()
     }
 }
 
 #[cfg(test)]
 mod tests { 
-    use crate::ext::LinkExt;
-
     use super::*;
 
     #[test]
-    fn link() { 
+    fn inv_e() { 
         let l = Link::from_pd_code([[1,5,2,4],[3,1,4,6],[5,3,6,2]]);
         let l = InvLink::new(l, [(1,5), (2,4)]);
-        println!("{:?}", l);
+
+        assert_eq!(l.inv_e(1), 5);
+        assert_eq!(l.inv_e(2), 4);
+        assert_eq!(l.inv_e(3), 3);
+        assert_eq!(l.inv_e(4), 2);
+        assert_eq!(l.inv_e(5), 1);
+        assert_eq!(l.inv_e(6), 6);
+    }
+    
+    #[test]
+    fn inv_x() { 
+        let l = Link::from_pd_code([[1,5,2,4],[3,1,4,6],[5,3,6,2]]);
+        let l = InvLink::new(l, [(1,5), (2,4)]);
+
+        assert_eq!(l.inv_x(0), 0);
+        assert_eq!(l.inv_x(1), 2);
+        assert_eq!(l.inv_x(2), 1);
     }
 }
