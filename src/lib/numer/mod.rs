c956fa53b47640e8448fd068c759e50b448047d3
Taketo Sano <taketo1024@gmail.com>
2024-03-19 17:31:00 +0900

    improve ssi.

--- src/lib/numer/mod.rs
+++ src/lib/numer/mod.rs
@@ -1,4 +1,5 @@
 mod misc;
 mod ss;
+mod ssi;
 
 pub use ss::{ss_invariant, ss_invariant_v1, ss_invariant_v2};
--- src/lib/numer/ssi.rs
+++ src/lib/numer/ssi.rs
@@ -4,24 +4,23 @@
 
 use core::panic;
 
-use itertools::Itertools;
 use log::info;
 use yui_homology::{ChainComplexTrait, RModStr};
-use yui_link::Link;
 use yui_homology::utils::{ChainReducer, HomologyCalc};
 use yui::{EucRing, EucRingOps};
 use yui_matrix::sparse::SpMat;
 use yui_matrix::MatTrait;
 
 use crate::numer::misc::div_vec;
-use crate::{InvLink, KhComplex, KhIComplex};
+use crate::{InvLink, KhIComplex};
 
+#[allow(unused)]
 pub fn ssi_invariants<R>(l: &InvLink, c: &R) -> (i32, i32)
 where R: EucRing, for<'x> &'x R: EucRingOps<R> { 
     assert!(!c.is_zero());
     assert!(!c.is_unit());
 
-    info!("compute ssi, c = {c} ({}).", std::any::type_name::<R>());
+    info!("compute ssi, c = {c} over {}.", R::math_symbol());
 
     let (d0, d1) = div(l, c);
     let w = l.link().writhe();
@@ -30,142 +29,189 @@ where R: EucRing, for<'x> &'x R: EucRingOps<R> {
     let ss0 = 2 * d0 + w - r + 1;
     let ss1 = 2 * d1 + w - r + 1;
 
-    info!("d0 = {d0}, d1 = {d1}, w = {w}, r = {r}.");
-    info!("ssi = ({ss0}, {ss1}) c = {c} ({}).", std::any::type_name::<R>());
+    info!("w = {w}, r = {r}.");
+    info!("ssi = ({ss0}, {ss1}).");
 
     (ss0, ss1)
 }
 
 fn div<R>(l: &InvLink, c: &R) -> (i32, i32)
 where R: EucRing, for<'x> &'x R: EucRingOps<R> { 
-    let ckh = KhIComplex::new(l, c, &R::zero(), reduced);
+    let ckh = KhIComplex::new(l, c, true);
     let zs = ckh.canon_cycles();
 
-    // assert_eq!(zs.len(), r);
-    // assert!(zs.iter().all(|z| z.gens().all(|x| x.h_deg() == 0)));
+    assert_eq!(zs.len(), 2);
 
-    // let vs = zs.iter().map(|z| {
-    //     ckh[0].vectorize(z)
-    // }).collect_vec();
+    let z0 = &zs[0];
+    let z1 = &zs[1]; 
 
-    // let range = ckh.deg_shift().0 ..= 1;
-    // let mut reducer = ChainReducer::new(range.clone(), 1, true);
-    // for i in range { 
-    //     reducer.set_matrix(i, ckh.d_matrix(i))
-    // }
+    assert!(z0.gens().all(|x| x.h_deg() == 0));
+    assert!(z1.gens().all(|x| x.h_deg() == 1));
 
-    // let vs = vs.iter().map(|v| 
-    //     reducer.trans(0).unwrap().forward(v)
-    // ).collect_vec();
+    let v0 = ckh[0].vectorize(&z0);
+    let v1 = ckh[1].vectorize(&z1);
 
-    // let d1 = reducer.matrix( 0).unwrap().clone();
-    // let d0 = reducer.matrix(-1).cloned().unwrap_or(SpMat::zero((d1.ncols(), 0)));
-    // let kh = HomologyCalc::calculate(d0, d1, true);
+    let range = -1 ..= 2;
+    let mut reducer = ChainReducer::new(range.clone(), 1, true);
+    for i in range { 
+        reducer.set_matrix(i, ckh.d_matrix(i))
+    }
 
-    // info!("homology: {}", kh.math_symbol());    
+    reducer.reduce_all(false);
+    reducer.reduce_all(true);
 
-    // assert_eq!(kh.rank(), r);
+    let v0 = reducer.trans(0).unwrap().forward(&v0);
+    let v1 = reducer.trans(1).unwrap().forward(&v1);
+
+    let d0 = reducer.matrix( 0).unwrap().clone();
+    let d1 = reducer.matrix( 1).unwrap().clone();
+    let dm = reducer.matrix(-1).cloned().unwrap_or(SpMat::zero((d0.ncols(), 0)));
+
+    let kh0 = HomologyCalc::calculate(dm, d0.clone(), true);
+    let kh1 = HomologyCalc::calculate(d0, d1, true);
+
+    info!("KhI[0]: {}", kh0.math_symbol());    
+    info!("KhI[1]: {}", kh1.math_symbol());    
+
+    assert_eq!(kh0.rank(), 1);
+    assert_eq!(kh1.rank(), 1);
     
-    // let t = kh.trans().unwrap();
-    // let vs = vs.iter().map(|v| {
-    //     t.forward(&v).subvec(0..r)
-    // }).collect_vec();
+    let v0 = kh0.trans().unwrap().forward(&v0).subvec(0..1);
+    let v1 = kh1.trans().unwrap().forward(&v1).subvec(0..1);
 
-    // let v = &vs[0];
-    // let Some(d) = div_vec(v, c) else { 
-    //     panic!("invalid divisibility for v = {}, c = {}", v, c)
-    // };
-    
-    // d
-    todo!()
+    info!("a0: {z0} -> [{}]", v0.to_dense()[0]);
+    info!("a1: {z1} -> [{}]", v1.to_dense()[0]);
+
+    let Some(d0) = div_vec(&v0, c) else { 
+        panic!("invalid divisibility for v = {}, c = {}", v0, c)
+    };
+    let Some(d1) = div_vec(&v1, c) else { 
+        panic!("invalid divisibility for v = {}, c = {}", v1, c)
+    };
+
+    info!("d0: {d0}");
+    info!("d1: {d1}");
+
+    (d0, d1)
 }
 
 #[cfg(test)]
 mod tests {
+    use yui::poly::Poly;
+    use yui::FF;
     use yui_link::Link;
+
     use super::*;
 
-    // #[test]
-    // fn test_unknot_rm1() { 
-    //     let l = Link::from_pd_code([[0,0,1,1]]);
-    //     test(&l, 2, 0);
-    // }
+    type R = FF<2>;
+    type P = Poly<'H', R>;
 
-    // #[test]
-    // fn test_unknot_rm1_neg() { 
-    //     let l = Link::from_pd_code([[0,1,1,0]]);
-    //     test(&l, 2, 0);
-    // }
+    #[test]
+    fn test_unknot_pos_twist() { 
+        let l = InvLink::new(
+            Link::from_pd_code([[0,0,1,1]]),
+            [],
+            Some(0)
+        );
+        let c = P::variable();
 
-    // #[test]
-    // fn test_3_1() { 
-    //     let l = Link::from_pd_code([[1,4,2,5],[3,6,4,1],[5,2,6,3]]);
-    //     test(&l, 2, -2);
-    // }
+        let div = div(&l, &c);
+        assert_eq!(div.0, 0);
+        assert_eq!(div.1, 0);
 
-    // #[test]
-    // fn test_4_1() { 
-    //     let l = Link::from_pd_code([[4,2,5,1],[8,6,1,5],[6,3,7,4],[2,7,3,8]]);
-    //     test(&l, 2, 0);
-    // }
+        let ssi = ssi_invariants(&l, &c);
+        assert_eq!(ssi.0, 0);
+        assert_eq!(ssi.1, 0);
+    }
 
-    // #[test]
-    // fn test_5_1() { 
-    //     let l = Link::from_pd_code([[1,6,2,7],[3,8,4,9],[5,10,6,1],[7,2,8,3],[9,4,10,5]]);
-    //     test(&l, 2, -4);
-    // }
+    #[test]
+    fn test_unknot_neg_twist() { 
+        let l = InvLink::new(
+            Link::from_pd_code([[0,1,1,0]]),
+            [],
+            Some(0)
+        );
+        let c = P::variable();
 
-    // #[test]
-    // fn test_5_2() { 
-    //     let l = Link::from_pd_code([[1,4,2,5],[3,8,4,9],[5,10,6,1],[9,6,10,7],[7,2,8,3]]);
-    //     test(&l, 2, -2);
-    // }
+        let div = div(&l, &c);
+        assert_eq!(div.0, 1);
+        assert_eq!(div.1, 1);
 
-    // #[test]
-    // fn test_6_1() { 
-    //     let l = Link::from_pd_code([[1,4,2,5],[7,10,8,11],[3,9,4,8],[9,3,10,2],[5,12,6,1],[11,6,12,7]]);
-    //     test(&l, 2, 0);
-    // }
+        let ssi = ssi_invariants(&l, &c);
+        assert_eq!(ssi.0, 0);
+        assert_eq!(ssi.1, 0);
+    }
 
-    // #[test]
-    // fn test_6_2() { 
-    //     let l = Link::from_pd_code([[1,4,2,5],[5,10,6,11],[3,9,4,8],[9,3,10,2],[7,12,8,1],[11,6,12,7]]);
-    //     test(&l, 2, -2);
-    // }
+    #[test]
+    fn test_unknot_neg_twist2() { 
+        let l = InvLink::new(
+            Link::from_pd_code([[0,1,3,0],[2,3,1,2]]),
+            [(1,3)],
+            Some(0)
+        );
+        let c = P::variable();
 
-    // #[test]
-    // fn test_6_3() { 
-    //     let l = Link::from_pd_code([[4,2,5,1],[8,4,9,3],[12,9,1,10],[10,5,11,6],[6,11,7,12],[2,8,3,7]]);
-    //     test(&l, 2, 0);
-    // }
+        let div = div(&l, &c);
+        assert_eq!(div.0, 2);
+        assert_eq!(div.1, 2);
 
-    // #[test]
-    // fn test_7_1() { 
-    //     let l = Link::from_pd_code([[1,8,2,9],[3,10,4,11],[5,12,6,13],[7,14,8,1],[9,2,10,3],[11,4,12,5],[13,6,14,7]]);
-    //     test(&l, 2, -6);
-    // }
+        let ssi = ssi_invariants(&l, &c);
+        assert_eq!(ssi.0, 0);
+        assert_eq!(ssi.1, 0);
+    }
 
-    // #[test]
-    // fn test_7_2() { 
-    //     let l = Link::from_pd_code([[1,4,2,5],[3,10,4,11],[5,14,6,1],[7,12,8,13],[11,8,12,9],[13,6,14,7],[9,2,10,3]]);
-    //     test(&l, 2, -2);
-    // }
+    #[test]
+    fn test_3_1() { 
+        let l = InvLink::new( // positive
+            Link::from_pd_code([[1,5,2,4],[3,1,4,6],[5,3,6,2]]), 
+            [(1,5), (2,4)],
+            Some(3)
+        );
+        let c = P::variable();
 
-    // #[test]
-    // fn test_7_3() { 
-    //     let l = Link::from_pd_code([[6,2,7,1],[10,4,11,3],[14,8,1,7],[8,14,9,13],[12,6,13,5],[2,10,3,9],[4,12,5,11]]);
-    //     test(&l, 2, 4);
-    // }
+        let div = div(&l, &c);
+        assert_eq!(div.0, 0);
+        assert_eq!(div.1, 0);
 
-    // #[test]
-    // fn test_8_19() { 
-    //     let l = Link::from_pd_code([[4,2,5,1],[8,4,9,3],[9,15,10,14],[5,13,6,12],[13,7,14,6],[11,1,12,16],[15,11,16,10],[2,8,3,7]]);
-    //     test(&l, 2, 6);
-    // }
+        let ssi = ssi_invariants(&l, &c);
+        assert_eq!(ssi.0, 2);
+        assert_eq!(ssi.1, 2);
+    }
 
-    // #[test]
-    // fn test_8_19_v1() { 
-    //     let l = Link::from_pd_code([[4,2,5,1],[8,4,9,3],[9,15,10,14],[5,13,6,12],[13,7,14,6],[11,1,12,16],[15,11,16,10],[2,8,3,7]]);
-    //     test_v1(&l, 2, 6);
-    // }
+    #[test]
+    fn test_3_1_m() { 
+        let l = InvLink::new( // negative
+            Link::from_pd_code([[1,5,2,4],[3,1,4,6],[5,3,6,2]]).mirror(), 
+            [(1,5), (2,4)],
+            Some(3)
+        );
+        let c = P::variable();
+
+        let div = div(&l, &c);
+        assert_eq!(div.0, 1);
+        assert_eq!(div.1, 1);
+
+        let ssi = ssi_invariants(&l, &c);
+        assert_eq!(ssi.0, -2);
+        assert_eq!(ssi.1, -2);
+    }
+
+    #[test]
+    fn test_9_46() { 
+        let l = InvLink::new(
+            Link::from_pd_code([[17,7,0,6],[12,5,13,6],[11,1,12,0],[7,17,8,16],[4,13,5,14],[1,11,2,10],[15,9,16,8],[14,3,15,4],[9,3,10,2]]), 
+            [(6,12),(7,11),(1,17),(5,13),(2,16),(8,10),(4,14),(3,15)],
+            Some(0)
+        );
+        println!("{:?}", l.link().components());
+        let c = P::variable();
+
+        let ssi = ssi_invariants(&l, &c);
+
+        assert_eq!(ssi.0, 0);
+        assert_eq!(ssi.1, 2);
+
+        // let ss = ss_invariant(l.link(), &c, true);
+        // assert_eq!(ss, 0);
+    }
 }
