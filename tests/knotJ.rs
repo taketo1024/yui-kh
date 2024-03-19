885f605b90bbccfccccf01f35e82618fde88de38
Taketo Sano <taketo1024@gmail.com>
2024-03-19 17:42:57 +0900

    add knotJ test.

---
+++ tests/knotJ.rs
@@ -0,0 +1,30 @@
+#![allow(non_snake_case)]
+use yui::poly::Poly;
+use yui::FF;
+use yui_kh::{ssi_invariants, InvLink};
+
+fn init_logger() { 
+    use simplelog::*;
+    TermLogger::init(
+        LevelFilter::Trace,
+        Config::default(),
+        TerminalMode::Mixed,
+        ColorChoice::Always
+    ).unwrap();
+}
+
+// cargo test -r -- --exact knotJ --nocapture --include-ignored
+#[test]
+#[ignore]
+fn knotJ() { 
+    type R = FF<2>;
+    type P = Poly<'H', R>;
+
+    init_logger();
+    
+    let l = InvLink::knotJ();
+    let c = P::variable();
+
+    let ssi = ssi_invariants(&l, &c);
+    println!("{:?}", ssi);
+}
