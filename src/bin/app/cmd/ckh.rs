a1e39828fda628c5e688d430196ba0ec7625c794
Taketo Sano <taketo1024@gmail.com>
2024-09-12 15:19:39 +0900

    Args impls Default.

--- src/bin/app/cmd/ckh.rs
+++ src/bin/app/cmd/ckh.rs
@@ -1,12 +1,12 @@
-use std::str::FromStr;
-use itertools::Itertools;
-use yui::{Ring, RingOps};
-use yui_homology::{DisplayTable, DisplaySeq, ChainComplexCommon};
-use yui_kh::KhComplex;
 use crate::app::utils::*;
+use itertools::Itertools;
+use std::str::FromStr;
+use yui::{Ring, RingOps};
+use yui_homology::{ChainComplexCommon, DisplaySeq, DisplayTable};
+use yui_kh::KhComplex;
 
-#[derive(Debug, clap::Args)]
-pub struct Args { 
+#[derive(Debug, clap::Args, Default)]
+pub struct Args {
     link: String,
 
     #[arg(short, long, default_value = "0")]
@@ -28,7 +28,7 @@ pub struct Args {
     no_simplify: bool,
 
     #[arg(long)]
-    pub debug: bool
+    pub debug: bool,
 }
 
 pub fn run(args: &Args) -> Result<String, Box<dyn std::error::Error>> {
@@ -36,26 +36,30 @@ pub fn run(args: &Args) -> Result<String, Box<dyn std::error::Error>> {
 }
 
 fn describe_ckh<R>(args: &Args) -> Result<String, Box<dyn std::error::Error>>
-where R: Ring + FromStr, for<'x> &'x R: RingOps<R> { 
+where
+    R: Ring + FromStr,
+    for<'x> &'x R: RingOps<R>,
+{
     let (h, t) = parse_pair::<R>(&args.c_value)?;
     let bigraded = h.is_zero() && t.is_zero();
 
-    if args.reduced && !t.is_zero() { 
+    if args.reduced && !t.is_zero() {
         return err!("{t} != 0 is not allowed for reduced.");
     }
-    
+
     let l = load_link(&args.link, args.mirror)?;
-    let c = if args.no_simplify { 
+    let c = if args.no_simplify {
         KhComplex::new_v1(&l, &h, &t, args.reduced)
-    } else { 
+    } else {
         KhComplex::new(&l, &h, &t, args.reduced)
     };
-    
-    let vs = if args.with_alpha { 
-        c.canon_cycles().iter().map(|z| {
-            (0, c[0].vectorize(z))
-        }).collect_vec()
-    } else { 
+
+    let vs = if args.with_alpha {
+        c.canon_cycles()
+            .iter()
+            .map(|z| (0, c[0].vectorize(z)))
+            .collect_vec()
+    } else {
         vec![]
     };
 
@@ -65,15 +69,22 @@ where R: Ring + FromStr, for<'x> &'x R: RingOps<R> {
         let c = c.into_bigraded();
         b.append(c.display_table("i", "j") + "\n");
         b.append(c.display_d() + "\n");
-    } else { 
+    } else {
         b.append(c.display_seq("i") + "\n");
         b.append(c.display_d() + "\n");
     }
 
-    if args.with_alpha { 
+    if args.with_alpha {
         b.append("\n");
         for (i, (_, v)) in vs.iter().enumerate() {
-            b.append(format!("a[{i}] = [{}]\n", v.to_dense().iter().map(|r| r.to_string()).collect_vec().join(", ")));
+            b.append(format!(
+                "a[{i}] = [{}]\n",
+                v.to_dense()
+                    .iter()
+                    .map(|r| r.to_string())
+                    .collect_vec()
+                    .join(", ")
+            ));
         }
     }
 
@@ -82,88 +93,69 @@ where R: Ring + FromStr, for<'x> &'x R: RingOps<R> {
 }
 
 #[cfg(test)]
-mod tests { 
+mod tests {
     use super::*;
 
     #[test]
-    fn test1() { 
+    fn test1() {
         let args = Args {
-        	link: "3_1".to_string(),
-        	c_value: "0".to_string(),
-        	c_type: CType::Z,
-        	mirror: false,
-        	reduced: false,
-        	with_alpha: false,
-            no_simplify: false,
-            debug: false
+            link: "3_1".to_string(),
+            c_value: "0".to_string(),
+            ..Default::default()
         };
         let res = run(&args);
         assert!(res.is_ok());
     }
 
     #[test]
-    fn test2() { 
+    fn test2() {
         let args = Args {
-        	link: "[[1,4,2,5],[3,6,4,1],[5,2,6,3]]".to_string(),
-        	c_value: "2".to_string(),
-        	c_type: CType::Z,
-        	mirror: true,
-        	reduced: true,
-        	with_alpha: true,
-            no_simplify: false,
-            debug: false
+            link: "[[1,4,2,5],[3,6,4,1],[5,2,6,3]]".to_string(),
+            c_value: "2".to_string(),
+            mirror: true,
+            reduced: true,
+            with_alpha: true,
+            ..Default::default()
         };
         let res = run(&args);
         assert!(res.is_ok());
     }
 
     #[cfg(feature = "poly")]
-    mod poly_tests { 
+    mod poly_tests {
         use super::*;
-        
+
         #[test]
-        fn test_zpoly_h() { 
+        fn test_zpoly_h() {
             let args = Args {
                 link: "3_1".to_string(),
                 c_value: "H".to_string(),
                 c_type: CType::Z,
-                mirror: false,
-                reduced: false,
-                with_alpha: false,
-                no_simplify: false,
-                debug: false
+                ..Default::default()
             };
             let res = run(&args);
             assert!(res.is_ok());
         }
 
         #[test]
-        fn test_zpoly_t() { 
+        fn test_zpoly_t() {
             let args = Args {
                 link: "3_1".to_string(),
                 c_value: "T".to_string(),
                 c_type: CType::Z,
-                mirror: false,
-                reduced: false,
-                with_alpha: false,
-                no_simplify: false,
-                debug: false
+                ..Default::default()
             };
             let res = run(&args);
             assert!(res.is_ok());
         }
 
         #[test]
-        fn test_zpoly_ht() { 
+        fn test_zpoly_ht() {
             let args = Args {
                 link: "3_1".to_string(),
                 c_value: "H,T".to_string(),
                 c_type: CType::Z,
-                mirror: false,
-                reduced: false,
-                with_alpha: false,
-                no_simplify: false,
-                debug: false
+                ..Default::default()
             };
             let res = run(&args);
             assert!(res.is_ok());
--- src/bin/app/utils/ctype.rs
+++ src/bin/app/utils/ctype.rs
@@ -1,8 +1,10 @@
 use clap::ValueEnum;
+use derive_more::Display;
 use serde::Deserialize;
 
-#[derive(Clone, ValueEnum, Debug, derive_more::Display, Deserialize)]
+#[derive(Clone, ValueEnum, Display, Debug, Deserialize, Default)]
 #[clap(rename_all="verbatim")]
 pub enum CType { 
-    Z, Q, F2, F3, Gauss, Eisen
+    #[default] Z, 
+    Q, F2, F3, Gauss, Eisen
 }
