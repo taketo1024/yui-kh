e779a809eed6528a6ff2bff4c99f7eb8a4e1fefe
Taketo Sano <taketo1024@gmail.com>
2024-09-12 22:09:53 +0900

    KhHomology::gen_table

--- src/lib/kh/homology.rs
+++ src/lib/kh/homology.rs
@@ -1,15 +1,21 @@
+use std::collections::HashMap;
 use std::ops::{RangeInclusive, Index};
 use delegate::delegate;
+use cartesian::cartesian;
 
-use yui_homology::{GridTrait, XHomology, XHomologySummand, XHomology2, isize2};
+use yui_homology::{isize2, Grid2, GridTrait, RModStr, SimpleRModStr, XHomology, XHomology2, XHomologySummand};
 use yui::{EucRing, EucRingOps};
 use yui_link::Link;
 
 use crate::KhGen;
 
+use super::complex::KhChainExt;
+
 pub struct KhHomology<R> 
 where R: EucRing, for<'x> &'x R: EucRingOps<R> {
-    inner: XHomology<KhGen, R>
+    inner: XHomology<KhGen, R>,
+    h_range: RangeInclusive<isize>,
+    q_range: RangeInclusive<isize>
 }
 
 impl<R> KhHomology<R> 
@@ -18,19 +24,60 @@ where R: EucRing, for<'x> &'x R: EucRingOps<R> {
         KhHomology::new_v2(l, h, t, reduced, with_trans)
     }
 
-    pub(crate) fn new_impl(inner: XHomology<KhGen, R>) -> Self { 
-        Self { inner }
+    pub(crate) fn new_impl(inner: XHomology<KhGen, R>, h_range: RangeInclusive<isize>, q_range: RangeInclusive<isize>) -> Self { 
+        Self { inner, h_range, q_range }
     }
 
     pub fn h_range(&self) -> RangeInclusive<isize> { 
-        let h = &self.inner;
-        let h_min = h.support().min().unwrap_or(0);
-        let h_max = h.support().max().unwrap_or(0);
-        h_min ..= h_max
+        self.h_range.clone()
+    }
+
+    pub fn q_range(&self) -> RangeInclusive<isize> { 
+        self.q_range.clone()
     }
 
     pub fn inner(&self) -> &XHomology<KhGen, R> { 
         &self.inner
+    }
+
+    pub fn gen_table(&self) -> Grid2<SimpleRModStr<R>> { 
+        // TODO: check with_trans = true
+
+        let h_range = self.h_range();
+        let q_range = self.q_range().step_by(2);
+        let support = cartesian!(h_range, q_range.clone()).map(|(i, j)| 
+            isize2(i, j)
+        );
+
+        let mut table: HashMap<isize2, (usize, Vec<R>)> = HashMap::new();
+        let e = (0, vec![]);
+
+        for i in self.support() { 
+            let h = &self[i];
+            let r = h.rank();
+            let t = h.tors().len();
+
+            for k in 0..r { 
+                let z = h.gen_chain(k);
+                let q = z.q_deg();
+                let e = table.entry(isize2(i, q)).or_insert_with(|| e.clone());
+                e.0 += 1;
+            }
+
+            for k in 0..t { 
+                let z = h.gen_chain(r + k);
+                let q = z.q_deg();
+                let e = table.entry(isize2(i, q)).or_insert_with(|| e.clone());
+                e.1.push(h.tors()[k].clone());
+            }
+        }
+
+        Grid2::generate(support, move |idx| { 
+            let Some(e) = table.remove(&idx) else { 
+                return SimpleRModStr::zero()
+            };
+            SimpleRModStr::new(e.0, e.1, None)
+        })
     }
 }
 
